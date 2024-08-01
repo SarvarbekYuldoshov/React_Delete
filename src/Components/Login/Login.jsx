c204d74b0a51ff50f19fa8188e39380e07d815f4
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-08-02 01:44:02 +0300

    salom

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [phone,setPhone] = useState('');
+    const [password,setPassword] = useState('');
+    const Registr = () => {
+        axios({
+            url:`https://autoapi.dezinfeksiyatashkent.uz/api/auth/signin/`,
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+        })
+    }
   return (
     <div>
       
