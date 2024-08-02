f089edec0b1a1f43ecc29bcf3f5d3eca754717d7
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-08-02 11:04:31 +0300

    salom

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { Input, Select } from 'antd';
+import { Button, Input, Select } from 'antd';
 import axios from 'axios';
 import React, { useState } from 'react'
 import { Form } from 'react-router-dom';
@@ -6,6 +6,7 @@ import { Form } from 'react-router-dom';
 const Login = () => {
     const [phone,setPhone] = useState('');
     const [password,setPassword] = useState('');
+    const navigate = () =>()
     const Registr = () => {
         axios({
             url:`https://autoapi.dezinfeksiyatashkent.uz/api/auth/signin/`,
@@ -17,63 +18,27 @@ const Login = () => {
     }
   return (
     <div>
-        <Form
-    name="basic"
-    labelCol={{
-      span: 8,
-    }}
-    wrapperCol={{
-      span: 16,
-    }}
-    style={{
-      maxWidth: 600,
-    }}
-    initialValues={{
-      remember: true,
-    }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item
-      label="Username"
-      name="username"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your username!',
-        },
-      ]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item
-      label="Password"
-      name="password"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your password!',
-        },
-      ]}
-    >
-      <Input.Password />
-    </Form.Item>
-
-    <Form.Item
-      name="remember"
-      valuePropName="checked"
-      wrapperCol={{
-        offset: 8,
-        span: 16,
-      }}
-    >
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
+        <Form name="basic"labelCol={{span: 8,}}wrapperCol={{span: 16,}}style={{maxWidth: 600,}}initialValues={{remember: true,}}autoComplete="off">
+            <Form.Item label="Username"name="username"rules={[
+                {
+                    required: true,
+                    message: 'Please input your username!',
+                },
+            ]}>
+                    <Input />
+            </Form.Item>
+            <Form.Item label="Password"name="password"rules={[
+                {
+                    required: true,
+                    message: 'Please input your password!',
+                },
+            ]}>
+            <Input.Password />
+            </Form.Item>
+            <Form.Item name="remember"valuePropName="checked"wrapperCol={{offset: 8,span: 16,}}>
+                    <Button type="primary" htmlType="submit">Submit</Button>
+            </Form.Item>
+        </Form>
     </div>
   )
 }
