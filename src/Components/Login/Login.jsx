6878b4b08e441b22bf25d36640ad95559362d39c
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-08-02 05:21:06 +0300

    salom

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,5 +1,7 @@
+import { Input, Select } from 'antd';
 import axios from 'axios';
 import React, { useState } from 'react'
+import { Form } from 'react-router-dom';
 
 const Login = () => {
     const [phone,setPhone] = useState('');
@@ -15,7 +17,63 @@ const Login = () => {
     }
   return (
     <div>
-      
+        <Form
+    name="basic"
+    labelCol={{
+      span: 8,
+    }}
+    wrapperCol={{
+      span: 16,
+    }}
+    style={{
+      maxWidth: 600,
+    }}
+    initialValues={{
+      remember: true,
+    }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item
+      label="Username"
+      name="username"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your username!',
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="Password"
+      name="password"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your password!',
+        },
+      ]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+    <Form.Item
+      name="remember"
+      valuePropName="checked"
+      wrapperCol={{
+        offset: 8,
+        span: 16,
+      }}
+    >
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
     </div>
   )
 }
